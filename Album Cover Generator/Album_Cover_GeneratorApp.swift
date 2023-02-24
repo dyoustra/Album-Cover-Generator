040f60896fc5e0be75e1816b21f2ed3435e28589
Danny Youstra <danny@youstra.com>
2023-02-24 13:02:53 -0800

    Initial Commit

---
+++ Album Cover Generator/Album_Cover_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Album_Cover_GeneratorApp.swift
+//  Album Cover Generator
+//
+//  Created by Danny Youstra on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Album_Cover_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
