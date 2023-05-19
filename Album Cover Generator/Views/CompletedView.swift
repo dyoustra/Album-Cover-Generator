1562efe2b94236f0f8da682f8c1e8959266bac38
rehanparwani <rehanparwani@gmail.com>
2023-05-18 20:28:36 -0700

    Splash Screen and App Logo Updated and Share Sheet Done

--- Album Cover Generator/Views/CompletedView.swift
+++ Album Cover Generator/Views/CompletedView.swift
@@ -16,7 +16,13 @@ struct CompletedView: View {
     let collectionName : String
     let isPlaylist : Bool
     
+    @State var items : [Any] = []
+    
+    @State var displayShareSheet = false
+    
     @Binding var displayCompletedView : Bool
+    
+    @State private var renderedImage: Image?
     
     var body: some View {
         VStack {
@@ -26,43 +32,50 @@ struct CompletedView: View {
                 .cornerRadius(50)
                 .shadow(radius: 20)
                 .padding()
+                .onAppear {
+                    let renderer = ImageRenderer(content: coverImage)
+                    renderer.scale = 3
+                    if let image = renderer.cgImage {
+                        renderedImage = Image(decorative: image, scale: 1.0)
+                    }
+                }
             Text(collectionName)
                 .font(.largeTitle)
                 .bold()
             
-            Button(action: {
-                
-            }) {
-                HStack {
-                    Text("Show Diffusion Steps")
-                    Image(systemName: "photo.on.rectangle.angled")
-                }
-                .frame(maxWidth: .infinity)
-                .foregroundColor(colorScheme == .dark ? .black : .white)
-                .padding(10)
-                .bold()
-            }
-            .background(Color.primary)
-            .cornerRadius(10)
-            .padding(5)
-            
-            if (isPlaylist) {
-                Button(action: {
-                    
-                }) {
-                    HStack {
-                        Text("Upload to Spotify")
-                        Image(systemName: "music.note")
-                    }
-                    .frame(maxWidth: .infinity)
-                    .foregroundColor(colorScheme == .dark ? .black : .white)
-                    .padding(10)
-                    .bold()
-                }
-                .background(Color.primary)
-                .cornerRadius(10)
-                .padding(5)
-            }
+//            Button(action: {
+//                
+//            }) {
+//                HStack {
+//                    Text("Show Diffusion Steps")
+//                    Image(systemName: "photo.on.rectangle.angled")
+//                }
+//                .frame(maxWidth: .infinity)
+//                .foregroundColor(colorScheme == .dark ? .black : .white)
+//                .padding(10)
+//                .bold()
+//            }
+//            .background(Color.primary)
+//            .cornerRadius(10)
+//            .padding(5)
+//            
+//            if (isPlaylist) {
+//                Button(action: {
+//                    
+//                }) {
+//                    HStack {
+//                        Text("Upload to Spotify")
+//                        Image(systemName: "music.note")
+//                    }
+//                    .frame(maxWidth: .infinity)
+//                    .foregroundColor(colorScheme == .dark ? .black : .white)
+//                    .padding(10)
+//                    .bold()
+//                }
+//                .background(Color.primary)
+//                .cornerRadius(10)
+//                .padding(5)
+//            }
             
             Button(action: {
                 displayCompletedView = false
@@ -78,12 +91,12 @@ struct CompletedView: View {
             .padding(5)
         }
         .navigationTitle(!isPlaylist ? Text("Album Cover") : Text("Playlist Cover"))
-        .navigationBarItems(trailing:
-            Button(action: {
-                
-            }) {
-                Image(systemName: "square.and.arrow.up")
+        .toolbar {
+            if let renderedImage {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    ShareLink("Share", item: renderedImage, subject: Text("Share Generated Image"),  message: Text(""), preview: SharePreview(Text(collectionName), image: renderedImage))
+                }
             }
-        )
+        }
     }
 }
